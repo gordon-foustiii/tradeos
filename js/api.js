539a71034c97f0058f1789d1cb9d588567141ebf
gordon-foustiii <gordon.foustiii@gmail.com>
2026-04-16 23:46:34 -0400

    update

--- js/api.js
+++ js/api.js
@@ -1,19 +1,20 @@
 var BASE_URL = 'https://script.google.com/macros/s/AKfycbzZ1rHLUPvFs7Qr-F2IWi2Z3yE7myADX60hX2TVwg0CFGVSdcGYDDXLEXxt3g26S9YJ/exec';
-var API_URL = 'https://corsproxy.io/?' + encodeURIComponent(BASE_URL);
 
 function apiGet(action) {
-  return fetch('https://corsproxy.io/?' + encodeURIComponent(BASE_URL + '?action=' + action))
+  return fetch('https://api.allorigins.win/get?url=' + encodeURIComponent(BASE_URL + '?action=' + action))
     .then(function(res) { return res.json(); })
+    .then(function(data) { return JSON.parse(data.contents); })
     .catch(function(e) { console.error('API error:', e); return null; });
 }
 
 function apiPost(action, payload) {
   payload.action = action;
-  return fetch('https://corsproxy.io/?' + encodeURIComponent(BASE_URL), {
+  return fetch('https://api.allorigins.win/get?url=' + encodeURIComponent(BASE_URL), {
     method: 'POST',
     body: JSON.stringify(payload)
   })
     .then(function(res) { return res.json(); })
+    .then(function(data) { return JSON.parse(data.contents); })
     .catch(function(e) { console.error('API error:', e); return null; });
 }
 

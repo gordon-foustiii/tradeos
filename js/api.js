7400601ef03842c9f2772701f0df8c42c802c8a6
gordon-foustiii <gordon.foustiii@gmail.com>
2026-04-16 23:43:17 -0400

    Fix proxy URL encoding

--- js/api.js
+++ js/api.js
@@ -1,16 +1,16 @@
-var API_URL = 'https://corsproxy.io/?' + encodeURIComponent('https://script.google.com/macros/s/AKfycbzZ1rHLUPvFs7Qr-F2IWi2Z3yE7myADX60hX2TVwg0CFGVSdcGYDDXLEXxt3g26S9YJ/exec');
+var BASE_URL = 'https://script.google.com/macros/s/AKfycbzZ1rHLUPvFs7Qr-F2IWi2Z3yE7myADX60hX2TVwg0CFGVSdcGYDDXLEXxt3g26S9YJ/exec';
+var API_URL = 'https://corsproxy.io/?' + encodeURIComponent(BASE_URL);
 
 function apiGet(action) {
-  return fetch(API_URL + '?action=' + action, { redirect: 'follow' })
+  return fetch('https://corsproxy.io/?' + encodeURIComponent(BASE_URL + '?action=' + action))
     .then(function(res) { return res.json(); })
     .catch(function(e) { console.error('API error:', e); return null; });
 }
 
 function apiPost(action, payload) {
   payload.action = action;
-  return fetch(API_URL, {
+  return fetch('https://corsproxy.io/?' + encodeURIComponent(BASE_URL), {
     method: 'POST',
-    redirect: 'follow',
     body: JSON.stringify(payload)
   })
     .then(function(res) { return res.json(); })

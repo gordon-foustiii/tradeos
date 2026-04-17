edb6ccb5d67b963ee56bf1898989db74bf2d02f8
gordon-foustiii <gordon.foustiii@gmail.com>
2026-04-16 23:30:27 -0400

    Fix api.js syntax

--- js/api.js
+++ js/api.js
@@ -1,33 +1,22 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbzZ1rHLUPvFs7Qr-F2IWi2Z3yE7myADX60hX2TVwg0CFGVSdcGYDDXLEXxt3g26S9YJ/exec';
+var API_URL = 'https://script.google.com/macros/s/AKfycbzZ1rHLUPvFs7Qr-F2IWi2Z3yE7myADX60hX2TVwg0CFGVSdcGYDDXLEXxt3g26S9YJ/exec';
 
-async function apiGet(action) {
-  try {
-    const res = await fetch(`${API_URL}?action=${action}`, {
-      redirect: 'follow'
-    });
-    const data = await res.json();
-    return data;
-  } catch(e) {
-    console.error('API GET error:', e);
-    return null;
-  }
+function apiGet(action) {
+  return fetch(API_URL + '?action=' + action, { redirect: 'follow' })
+    .then(function(res) { return res.json(); })
+    .catch(function(e) { console.error('API error:', e); return null; });
 }
 
-async function apiPost(action, payload) {
-  try {
-    const res = await fetch(API_URL, {
-      method: 'POST',
-      redirect: 'follow',
-      body: JSON.stringify({ action, ...payload })
-    });
-    const data = await res.json();
-    return data;
-  } catch(e) {
-    console.error('API POST error:', e);
-    return null;
-  }
+function apiPost(action, payload) {
+  payload.action = action;
+  return fetch(API_URL, {
+    method: 'POST',
+    redirect: 'follow',
+    body: JSON.stringify(payload)
+  })
+    .then(function(res) { return res.json(); })
+    .catch(function(e) { console.error('API error:', e); return null; });
 }
 
-async function getStats() { return await apiGet('stats'); }
-async function getTrades() { return await apiGet('trades'); }
-async function submitTrade(data) { return await apiPost('submit', data); }
+function getStats() { return apiGet('stats'); }
+function getTrades() { return apiGet('trades'); }
+function submitTrade(data) { return apiPost('submit', data); }
